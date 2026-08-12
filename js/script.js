315427d8d73d789ef8ed3377a35a520d56978d6f
matiascastillo2005-glitch <matias.castillo2005@gmail.com>
2026-08-11 21:13:51 -0300

    feat(js): asignar imagen de youtube al logo Closes #4

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const tarjeta = document.getElementById('tarjeta');
 tarjeta.setAttribute('class', 'card');
 
 //2- Agregar a la imagen, el atributo "src" con el valor "https://www.youtube.com/img/desktop/yt_1200.png"
-
+const logo = document.getElementById('logo');
+logo.setAttribute('src', 'https://www.youtube.com/img/desktop/yt_1200.png');
 
 //3- Quitarle al titulo la clase que le está dando un formato feo
 
