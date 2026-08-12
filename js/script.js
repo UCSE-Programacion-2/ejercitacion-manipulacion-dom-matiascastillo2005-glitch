c95ec3010cbe8604b2c51cc1e0884f9f313045f8
matiascastillo2005-glitch <matias.castillo2005@gmail.com>
2026-08-11 21:19:01 -0300

    feat(js): verificar href del link youtube con hasAttribute Closes #6

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ const titulo = document.querySelector('h1');
 titulo.removeAttribute('class');
 
 //4- Chequear si el link a youtube posee o no el atributo href
-
+const linkYoutube = document.getElementById('link_youtube');
+const tieneHref = linkYoutube.hasAttribute('href');
+console.log(tieneHref);
 
 //5- Obtener el href del link a wikipedia y mostrarlo por consola
